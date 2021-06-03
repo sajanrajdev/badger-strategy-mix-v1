3aa24ace7af7a1d7e6d822d99df56a46956baa6c
Entreprenerd <valeranijr@gmail.com>
2021-06-03 16:28:29 +0200

    feat: strategy with comments

--- contracts/Strategy.sol
+++ contracts/Strategy.sol
@@ -3,11 +3,14 @@
 pragma solidity ^0.6.11;
 pragma experimental ABIEncoderV2;
 
-import "@openzeppelin/contracts-upgradeable/token/ERC20/IERC20Upgradeable.sol";
-import "@openzeppelin/contracts-upgradeable/math/SafeMathUpgradeable.sol";
-import "@openzeppelin/contracts-upgradeable/math/MathUpgradeable.sol";
-import "@openzeppelin/contracts-upgradeable/utils/AddressUpgradeable.sol";
-import "@openzeppelin/contracts-upgradeable/token/ERC20/SafeERC20Upgradeable.sol";
+import "../deps/@openzeppelin/contracts-upgradeable/token/ERC20/IERC20Upgradeable.sol";
+import "../deps/@openzeppelin/contracts-upgradeable/math/SafeMathUpgradeable.sol";
+import "../deps/@openzeppelin/contracts-upgradeable/math/MathUpgradeable.sol";
+import "../deps/@openzeppelin/contracts-upgradeable/utils/AddressUpgradeable.sol";
+import "../deps/@openzeppelin/contracts-upgradeable/token/ERC20/SafeERC20Upgradeable.sol";
+
+import "../interfaces/badger/IController.sol";
+
 
 import {
     BaseStrategy
@@ -18,9 +21,20 @@ contract StrategyHarvestMetaFarm is BaseStrategy {
     using AddressUpgradeable for address;
     using SafeMathUpgradeable for uint256;
 
-    address immutable lpComponent; // Token we provide liquidity with
-    address immutable reward; // Token we farm and swap to want / lpComponent
+    address public lpComponent; // Token we provide liquidity with
+    address public reward; // Token we farm and swap to want / lpComponent
 
+    /// @dev struct used for Harvest reporting, adapt to specific strat
+    struct HarvestData {
+        uint256 rewardHarvested;
+        uint256 keepReward;
+        uint256 rewardRecycled;
+        uint256 lpComponentDeposited;
+        uint256 wantProcessed;
+        uint256 wantDeposited;
+        uint256 governancePerformanceFee;
+        uint256 strategistPerformanceFee;
+    }
 
     function initialize(
         address _governance,
@@ -43,17 +57,25 @@ contract StrategyHarvestMetaFarm is BaseStrategy {
         withdrawalFee = _feeConfig[2];
 
         /// @dev do one off approvals here
-        IERC20Upgradeable(want).safeApprove(gauge, type(uint256).max);
+        // IERC20Upgradeable(want).safeApprove(gauge, type(uint256).max);
     }
 
     /// ===== View Functions =====
 
+    // @dev Specify the name of the strategy
     function getName() external override pure returns (string memory) {
         return "StrategyName";
     }
 
+
+    // @dev Specify the version of the Strategy, for upgrades
     function version() external pure returns (string memory) {
         return "1.0";
+    }
+
+    /// @dev Balance of want currently held in strategy positions
+    function balanceOfPool() public override view returns (uint256) {
+        return 0;
     }
 
     // @dev These are the tokens that cannot be moved except by the vault
@@ -72,6 +94,7 @@ contract StrategyHarvestMetaFarm is BaseStrategy {
 
     /// ===== Internal Core Implementations =====
 
+    /// @dev security check to avoid moving tokens that would cause a rugpull, edit based on strat
     function _onlyNotProtectedTokens(address _asset) internal override {
         require(address(want) != _asset, "want");
         require(lpComponent != _asset, "lpComponent");
@@ -79,12 +102,14 @@ contract StrategyHarvestMetaFarm is BaseStrategy {
     }
 
 
+    /// @dev deposit the amount of want
     function _deposit(uint256 _want) internal override {
     }
 
+    /// @dev utility function to withdraw all that a account has deposited
     function _withdrawAll() internal override {
     }
-
+    /// @dev withdraw the specified amount of want
     function _withdrawSome(uint256 _amount) internal override returns (uint256) {
 
         return _amount;
@@ -96,15 +121,20 @@ contract StrategyHarvestMetaFarm is BaseStrategy {
 
         HarvestData memory harvestData;
 
+        uint256 _before = IERC20Upgradeable(want).balanceOf(address(this));
+
         /// @dev Write your code here
 
-
+        
+        /// @dev Harvest event that every strategy MUST have
         emit Harvest(harvestData.wantProcessed.sub(_before), block.number);
 
         return harvestData;
     }
 
     /// ===== Internal Helper Functions =====
+    
+    /// @dev used to manage the governance and strategist fee, make sure to use it to get paid!
     function _processPerformanceFees(uint256 _amount) internal returns (uint256 governancePerformanceFee, uint256 strategistPerformanceFee) {
         governancePerformanceFee = _processFee(want, _amount, performanceFeeGovernance, IController(controller).rewards());
 
